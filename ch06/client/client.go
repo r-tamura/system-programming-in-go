7e26a3d7638c36a1e39132525b24e5b1fe7d5c2e
r-tamura <rtamura30@gmail.com>
2018-07-29 19:21:31 +0900

    Fix Chapter 7 HTTP pipeline

--- ch06/client/client.go
+++ ch06/client/client.go
@@ -3,92 +3,75 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
-	"strconv"
-	"strings"
 )
 
 func main() {
 
 	sendMessages := []string{
-		"ASCII",
+		"ASCII", "PROGRAMMING", "PLUS",
 	}
 
 	current := 0
 	var connection net.Conn
-	// A loop for retry
-	for {
-		var err error
-		// when a connection is not created yet or retrying for error, start with dial up.
-		if connection == nil {
-			connection, err = net.Dial("tcp", "localhost:8080")
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Access: %d\n", current)
-		}
-
-		// Fetch from the server. If timeout has caused, retry, because an error is emmited in this line.
-		request, err := http.NewRequest("POST", "http://localhost:8080", strings.NewReader(sendMessages[current]))
+	var err error
+	requests := make(chan *http.Request, len(sendMessages))
+	// when a connection is not created yet or retrying for error, start with dial up.
+	if connection == nil {
+		connection, err = net.Dial("tcp", "localhost:8080")
 		if err != nil {
 			panic(err)
 		}
-		request.Header.Set("Accept-Encoding", "gzip")
+		fmt.Printf("Access: %d\n", current)
+	}
+
+	// A loop for retry
+	for i := 0; i < len(sendMessages); i++ {
+		// 最後のリクエストであるか
+		lastMessage := i == len(sendMessages)-1
+		request, err := http.NewRequest("GET", "http://localhost:8080?message="+sendMessages[i], nil)
+
+		// 最後のリクエストの場合はコネクションを閉じる
+		if lastMessage {
+			request.Header.Add("Connection", "close")
+		} else {
+			request.Header.Add("Connection", "keep-alive")
+		}
+
+		if err != nil {
+			panic(err)
+		}
 
 		err = request.Write(connection)
 		if err != nil {
 			panic(err)
 		}
-		reader := bufio.NewReader(connection)
+		fmt.Println("send: ", sendMessages[i])
+		requests <- request
+	}
 
-		response, err := http.ReadResponse(bufio.NewReader(connection), request)
-		if err != nil {
-			fmt.Println("Retry")
-			connection = nil
-			continue
-		}
+	close(requests)
 
-		dump, err := httputil.DumpResponse(response, false)
+	// Recieve all response at once
+	reader := bufio.NewReader(connection)
+
+	for request := range requests {
+		response, err := http.ReadResponse(reader, request)
 		if err != nil {
 			panic(err)
 		}
+		dump, err := httputil.DumpResponse(response, true)
+		if err != nil {
+			panic(err)
+		}
+
 		fmt.Println(string(dump))
-
-		// Chunked response
-		if len(response.TransferEncoding) < 1 || response.TransferEncoding[0] != "chunked" {
-			panic("Wrng transfer encoding")
-		}
-
-		for {
-			// Get the size of a chunk
-			sizeStr, err := reader.ReadBytes([]byte("\n")[0])
-			if err == io.EOF {
-				break
-			}
-			// Parse the size in hex. If the size is 0, close.
-			size, err := strconv.ParseInt(string(sizeStr[:len(sizeStr)-2]), 16, 64)
-			if size == 0 {
-				break
-			}
-			if err != nil {
-				panic(err)
-			}
-
-			// Allocate memory only the size
-			line := make([]byte, int(size))
-			reader.Read(line)
-			// 2続く改行コードの2つ目をスキップする
-			reader.Discard(2)
-			fmt.Printf("  %d bytes: %s\n", size, string(line))
-		}
-
-		// If all request is sent, exit
 		current++
 		if current == len(sendMessages) {
 			break
 		}
 	}
+
 }
--- ch06/server/server.go
+++ ch06/server/server.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -11,21 +12,49 @@ import (
 	"time"
 )
 
-var contents = []string{
-	"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-	"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-	"when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-	"It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-	"It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ",
-	"and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+// 順番に従ってconnnectionへwriteする(this func should be invoked as go routine)
+func writeToConn(sessionResponses chan chan *http.Response, connection net.Conn) {
+	defer connection.Close()
+	// Take responses in turn
+	for sessionResponse := range sessionResponses {
+		// Wait for the selected work
+		response := <-sessionResponse
+		response.Write(connection)
+		close(sessionResponse)
+	}
 }
 
-func isGZipAcceptable(request *http.Request) bool {
-	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+func handleRequest(request *http.Request, resultReceiver chan *http.Response) {
+	// デバッグ 第二引数をtrueにすることでbodyもダンプする
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+
+	content := "<h1>Hello, Golang</h1>\n"
+	response := &http.Response{
+		StatusCode:    200,
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		ContentLength: int64(len(content)),
+		Body:          ioutil.NopCloser(strings.NewReader(content)),
+	}
+
+	// Write to a channel ffter a process finished and
+	// restart the blocked writeToConn process
+	resultReceiver <- response
 }
 
 func processSession(connection net.Conn) {
 	fmt.Printf("Accept %v\n", connection.RemoteAddr())
+
+	// Channel for processing requests in right order in an session
+	sessionResponses := make(chan chan *http.Response, 50)
+	defer close(sessionResponses)
+
+	// Goroutine to serialize responses and write to socket.
+	go writeToConn(sessionResponses, connection)
 
 	// To loop for responding all time after acceptance
 	for {
@@ -46,28 +75,10 @@ func processSession(connection net.Conn) {
 			}
 			panic(err)
 		}
-
-		// デバッグ 第二引数をtrueにすることでbodyもダンプする
-		dump, err := httputil.DumpRequest(request, true)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(string(dump))
-
-		fmt.Fprint(connection, strings.Join([]string{
-			"HTTP/1.1 200 OK",
-			"Centent-Type: text/plain",
-			"Transfer-Encoding: chunked",
-			"", "",
-		}, "\r\n"))
-
-		// Write response body
-		for _, content := range contents {
-			bytes := []byte(content)
-			fmt.Fprintf(connection, "%x\r\n%s\r\n", len(bytes), content)
-		}
-
-		fmt.Fprintf(connection, "0\r\n\r\n")
+		sessionResponse := make(chan *http.Response)
+		sessionResponses <- sessionResponse
+		// Run async response
+		go handleRequest(request, sessionResponse)
 	}
 }
 
